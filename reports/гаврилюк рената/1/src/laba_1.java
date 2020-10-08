55e9cf8c4aeb63ccb16244be8857ae81e65401ce
Renata <chromic507@gmail.com>
2020-10-09 00:39:55 +0300

    исправлено задание 1 лр

--- reports/гаврилюк рената/1/src/laba_1.java
+++ reports/гаврилюк рената/1/src/laba_1.java
@@ -2,14 +2,6 @@ import java.util.Random;
 import java.util.Scanner;
 
 class Lab_1 {
-    public void randomOutput(int N) {
-        Random rand = new Random();
-        for(int i = 0; i < N; i++) {
-            System.out.println(rand.nextInt(100));
-        }
-        System.out.println();
-    }
-
     public void printMatrix (double[][] matrix) {
         for(int i = 0; i < matrix.length; i++) {
             for(int j = 0; j < matrix[i].length; j++) {
@@ -45,71 +37,36 @@ class Lab_1 {
         return abbreviate;
     }
 
-    public int hash(String str){
-        int hash = 7;
-        for (int i = 0; i < str.length(); i++) {
-            hash = hash * 31 + str.toCharArray()[i];
-        }
-        return hash;
-    }
-
-    public int[] sort(int[] set){
-        for (int i = 0; i < set.length; ++i){	        
-            for (int j = 0; j < set.length - 1; ++j){
-                if (set[j] > set[j + 1]){
-                    int temp = set[j];
-                    set[j] = set[j + 1];
-                    set[j + 1] = temp;
-                }
-            }
-            
-        }
-        return set;
-    }
-    public String[] deleteElement(String[] arr, String el){
-        String[] newSet = new String[arr.length - 1];
+    public String[] deleteElement(String[] arr, int pos){
+        String[] newArr = new String[arr.length - 1];
         for(int i = 0, j = 0; i < arr.length; ++i, ++j){
-            if(arr[i]!=null){
-            if(arr[i].equals(el)){
+            if(i == pos){
                 ++i;
             }
-            if(arr.length - 1 == i){
-                
-            }else{
-                
-            newSet[j] = arr[i];}}
+            newArr[j] = arr[i];
         }
-        arr = newSet;
-        return arr;
+        return newArr;
     }
+
     public static void main(String[] args) { 
         Lab_1 contr = new Lab_1();
         
         System.out.println("Task 1");
         Random rand = new Random();
-       // int hashArr[]
         int N = args.length;
-            for(int i = 0; i < N; i++) {
-                String el = args[rand.nextInt(args.length)];
-                System.out.println(el);
-                args = contr.deleteElement(args, el);
-                                //System.out.println(contr.hash(args[i]));
-               // System.out.println((args[i]));
-            }
-        //}
-
-        /*
-
-        System.out.println(); 
+        for(int i = 0; i < N - 1; ++i) {
+            int randNum = rand.nextInt(args.length - 1);
+            String el = args[randNum];
+            System.out.println(el);
+            args = contr.deleteElement(args, randNum);
+        }
+        System.out.println(args[0]);
         System.out.println(); 
         
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter number : ");
-        contr.randomOutput(in.nextInt());
-
         System.out.println("Task 2");
         System.out.print("Enter num rows : ");
-        int N = in.nextInt();
+        N = in.nextInt();
         System.out.print("Enter num colums : ");
         int M = in.nextInt();
 
@@ -132,5 +89,5 @@ class Lab_1 {
         System.out.println(contr.abbreviate(in.nextLine()));
         
         in.close();
-    */ }
+    }
 }
