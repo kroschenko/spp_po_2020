9fdb43bcd4f7cec53e2d84813d69d03fd3e09990
Viktoria Grigoryeva <viktoria.grig27@gmail.com>
2020-12-16 16:40:19 +0300

    fix report lab 3

--- reports/Григорьева Виктория Андреевна/3/src/task_1/src/com/company/IntegerSet.java
+++ reports/Григорьева Виктория Андреевна/3/src/task_1/src/com/company/IntegerSet.java
@@ -15,42 +15,33 @@ public class IntegerSet {
                 this.setOfIntegers.add(el);
         }
     }
-
     public ArrayList<Integer> intersections(IntegerSet set) {
         ArrayList<Integer> list = set.getSetOfIntegers();
         list.retainAll(this.setOfIntegers);
         return list;
     }
-
     public boolean contains(int item) {
         return this.setOfIntegers.contains(item);
     }
-
     public int getItemById(int id) {
         --id;
         return (Integer)this.setOfIntegers.get(id);
     }
-
     public void addItem(int item) {
         this.setOfIntegers.add(item);
     }
-
     public void deleteItemById(int id) {
         this.setOfIntegers.remove(id);
     }
-
     public ArrayList<Integer> getSetOfIntegers() {
         return this.setOfIntegers;
     }
-
     public void setSetOfIntegers(ArrayList<Integer> setOfIntegers) {
         this.setOfIntegers = setOfIntegers;
     }
-
     public String toString() {
         return "IntegerSet = " + this.setOfIntegers;
     }
-
     public boolean equals(Object o) {
         if (this == o) {
             return true;
