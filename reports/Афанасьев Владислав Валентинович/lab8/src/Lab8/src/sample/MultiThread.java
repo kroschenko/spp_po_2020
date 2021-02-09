715c3c4ffdab9f78b9e2e9539ed2a8af0dbf28e5
BlackNord <technic_pr@mail.ru>
2021-02-09 15:46:03 +0300

    lab8 add multithread and row

---
+++ reports/Афанасьев Владислав Валентинович/lab8/src/Lab8/src/sample/MultiThread.java
@@ -0,0 +1,40 @@
+package sample;
+
+public class MultiThread {
+    private CalculRow calcul;
+    private Thread thread;
+
+    public void start() {
+        if(calcul != null) {
+            thread.start();
+        }
+    }
+
+    public void stop() {
+        if(calcul != null) {
+            calcul.stopping = true;
+        }
+    }
+
+    public void resume() {
+        if(calcul != null) {
+            calcul.stopping = false;
+        }
+    }
+
+    public void interrupt() {
+        if(calcul != null) {
+            thread.interrupt();
+        }
+    }
+
+    public void setCalculator(CalculRow calcul) {
+        if (this.calcul != null) {
+            interrupt();
+        }
+
+        this.calcul = calcul;
+
+        thread = new Thread(calcul);
+    }
+}
