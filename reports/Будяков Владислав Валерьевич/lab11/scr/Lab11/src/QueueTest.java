519272586bf450b280e56f919be33631698172dc
Bydya13 <fakek2@yandex.by>
2021-05-04 15:12:35 +0300

    lab9,11

---
+++ reports/Будяков Владислав Валерьевич/lab11/scr/Lab11/src/QueueTest.java
@@ -0,0 +1,94 @@
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.NoSuchElementException;
+
+public class QueueTest {
+    private Queue<String> queue = new Queue<>();
+
+    @BeforeEach
+    public void before() {
+        queue.enqueue("1");
+        queue.enqueue("2");
+        queue.enqueue("3");
+    }
+
+    @AfterEach
+    public void after() {
+        queue.cleanUp();
+    }
+
+    @Test
+    public void isEmpty_SizeEqual3_False() {
+        assertFalse(queue.isEmpty());
+    }
+
+    @Test
+    public void isEmpty_SizeEqual0_True() {
+        queue.cleanUp();
+        assertTrue(queue.isEmpty());
+    }
+
+    @Test
+    public void size_SizeEqual3_Success() {
+        assertEquals(3, queue.size());
+    }
+
+    @Test
+    public void size_SizeEqual4_Success() {
+        queue.enqueue("4");
+        assertEquals(4, queue.size());
+    }
+
+    @Test
+    public void peek_QueueIsEmpty_ThrowException() throws NoSuchElementException {
+        Throwable thrown = assertThrows(NoSuchElementException.class, () -> {
+            queue.cleanUp();
+            queue.peek();
+        });
+        assertEquals(thrown.getClass(), NoSuchElementException.class);
+    }
+
+    @Test()
+    public void peek_QueueIsNotEmpty_Return3() {
+        assertEquals("3", queue.peek());
+    }
+
+    @Test
+    public void cleanUp_SizeEqual3_Success() {
+        assertEquals(3, queue.size());
+        queue.cleanUp();
+        assertEquals(0, queue.size());
+    }
+
+    @Test
+    public void enqueue_SizeEqual3_Success() {
+        assertEquals(3, queue.size());
+        queue.enqueue("4");
+        assertEquals(4, queue.size());
+    }
+
+    @Test
+    public void dequeue_SizeEqual3_Success() {
+        assertEquals("1", queue.dequeue());
+    }
+
+    @Test
+    public void dequeue_QueueIsEmpty_ThrowException() throws NoSuchElementException {
+        Throwable thrown = assertThrows(NoSuchElementException.class, () -> {
+            queue.cleanUp();
+            assertEquals(0, queue.size());
+            queue.dequeue();
+        });
+        assertEquals(thrown.getClass(), NoSuchElementException.class);
+    }
+
+    @Test
+    public void dequeue_SizeEqual1_Success() {
+        queue.cleanUp();
+        queue.enqueue("str");
+        assertEquals("str", queue.dequeue());
+        assertEquals(0, queue.size());
+    }
+}
