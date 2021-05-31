73d7f5fda562355293f900467ca8c5dbdcc48d32
Будяков Владислав <bydya13@MacBook-Pro-Budakov.local>
2021-05-31 20:49:17 +0300

    Lab9: Add lab9

---
+++ reports/Будяков Владислав Валерьевич/lab9/src/Lab9/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) throws java.io.IOException,
+            java.sql.SQLException {
+        Menu menu=new Menu();
+        menu.start();
+    }
+}
